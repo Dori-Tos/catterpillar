f235cdf86bd5f69142b5dc322b419c7379de3839
Yessboom <Tanguy.guissard@hotmail.be>
2024-10-21 14:56:56 +0200

    Added web interface (but don't seem to work yet)

--- WebInterface_Backend.js
+++ WebInterface_Backend.js
@@ -1,15 +1,43 @@
-    // Set up express with socket.io
-    const app = express();
-    const server = require("http").Server(app);
-    const io = require("socket.io")(server);
+const express = require('express');
+const mqtt = require('mqtt');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
 
-    // Connect to TTN and subscribe to your device
-    const client = mqtt.connect(...);
-    client.on('connect', () => {
-      client.subscribe('your-device');
-    });
+// Set up express with socket.io
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
-    // When TTN sends a message, notify the client via socket.io
-    client.on('message', (topic, message) => {
-      io.emit('event-name', message);
-    });
+// Serve static files (like HTML, CSS, frontend JS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Add your TTN credentials here
+const appId = 'caterpillar'; 
+const accessKey = 'NNSXS.34STHQSUPUGIOHQVJ77RSLR57GCVCSU25QNPQJI.EDVNYJQZMMFDN6GVBZ5QWZNMSMWFHCQX2HPYL6DWGDUJUTLYXGOA'; 
+
+// Connect to TTN and subscribe to your device
+const options = {
+  username: appId,
+  password: accessKey,
+  clientId: `mqtt_${Math.random().toString(16).slice(3)}`
+};
+
+const client = mqtt.connect(`mqtts://${appId}.eu1.cloud.thethings.network`, options);
+
+client.on('connect', () => {
+  const deviceTopic = `v3/${appId}/devices/caterpillar-end-device/up`; 
+  client.subscribe(deviceTopic, (err) => {
+    if (!err) {
+      console.log(`Subscribed to ${deviceTopic}`);
+    }
+  });
+});
+
+client.on('message', (topic, message) => {
+  io.emit('event-name', message.toString());
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
